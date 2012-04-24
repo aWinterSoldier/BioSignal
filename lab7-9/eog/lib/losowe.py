195540088ad78766339c246ca33796177fb97fc3
lap4bio1 <lap4bio1@lap4.(none)>
2012-04-24 10:12:31 +0200

    Modified image display.

--- lab7-9/eog/lib/losowe.py
+++ lab7-9/eog/lib/losowe.py
@@ -2,50 +2,60 @@ import numpy as np
 import pylab as py
 import random
 import time
+import os
 
-def loslista(dl):
-    lista = [random.randint(0,1) for i in xrange(dl)]
-    return lista
-py.figure(dpi = 120)
-py.ion()
-
-py.imshow(np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]]), cmap = py.cm.Blues, interpolation = 'nearest')
-time.sleep(2)
-
-punkty = []
-for i in range(75):
-    P = random.randint(0,3)
+class ImgDisplay(object):
+    def __init__(self):
+        pass
     
-    if P == 0:
-        M = np.array([[0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]])
-        T = 'u'
-    if P == 1:
-        M = np.array([[0,0,0,0,0], [0,0,0,0,0], [1,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]])
-        T = 'l'
-    if P == 2:
-        M = np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0]])
-        T = 'd'
-    if P == 3:
-        M = np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,1], [0,0,0,0,0], [0,0,0,0,0]])
-        
-        T = 'r'
-                
+    def SquareAnimation(self):    
+        py.figure(dpi = 150, figsize=(12,6))
+        py.ion()
+
+        py.imshow(np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]]), cmap = py.cm.Blues, interpolation = 'nearest')
+        time.sleep(2)
+
+        #punkty = []
+        for i in range(15):
+            P = random.randint(0,3)
             
-    py.clf()
-    py.imshow(M, cmap = py.cm.Blues, interpolation = 'nearest')
-    py.draw()
-    stop = time.time()
-    punkty.append((T,stop))   
-    time.sleep(2)
-    py.clf()
-    py.imshow(np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]]), cmap = py.cm.Blues, interpolation = 'nearest')
-    py.draw()
-    time.sleep(2)
-print punkty
-File_name = 'punkty.dat'    
-with open(File_name, 'w') as f:
-     for i in  punkty:
-        f.write("%s, %s \n" % i)
-    
-py.show()
+            if P == 0:
+                M = np.array([[0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]])
+                T = 'up'
+            if P == 1:
+                M = np.array([[0,0,0,0,0], [0,0,0,0,0], [1,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]])
+                T = 'left'
+            if P == 2:
+                M = np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0]])
+                T = 'down'
+            if P == 3:
+                M = np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,1], [0,0,0,0,0], [0,0,0,0,0]])
+                
+                T = 'right'
+                        
+                    
+            py.clf()
+            print T
+            py.imshow(M, cmap = py.cm.Blues, interpolation = 'nearest')
+            py.draw()
+            stop = time.time()
+            #punkty.append((T,stop))   
+            time.sleep(0.8)
+            
+            py.clf()
+            py.imshow(np.array([[0,0,0,0,0], [0,0,0,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0]]), cmap = py.cm.Blues, interpolation = 'nearest')
+            py.draw()
+            
+            
+            time.sleep(2)
+            os.system('clear')
+        #print punkty
+        #File_name = 'punkty.dat'    
+        #with open(File_name, 'w') as f:
+        #     for i in  punkty:
+        #        f.write("%s, %s \n" % i)
+            
+        py.show()
 
+a = ImgDisplay()
+a.SquareAnimation()
